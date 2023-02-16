dfc93872f0205f47082ecdf10e981cb835a67d27
ofcmail <ofcmailkom@gmail.com>
2023-02-16 20:39:59 +0300

    DOne

--- src/Main.java
+++ src/Main.java
@@ -1,29 +1,12 @@
-import java.util.ArrayList;
-import java.util.Arrays;
-import list.MyStringList;
+import list.*;
+
 public class Main {
     public static void main(String[] args) {
-
-        MyStringList myStringList = new MyStringList(10);
-        myStringList.add("1");
-        myStringList.add("2");
-        myStringList.add("3");
-        myStringList.add("4");
-        myStringList.add("5");
-        myStringList.add("6");
-        myStringList.add("7");
-        myStringList.add("8");
-        myStringList.add("9");
-        myStringList.add("10");
-        myStringList.remove("1");
-
-
-        myStringList.add(6, "7");
-        System.out.println(myStringList);
-
-        ArrayList<Integer> list = new ArrayList<>();
+        IntegerListImpl list = new IntegerListImpl(3);
         list.add(3);
-        list.add(2);
+        list.add(1);
+        list.add(5);
+        list.contains(5);
         System.out.println(list);
     }
 }
